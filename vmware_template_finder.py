c753535bb3173c4bb1c646bfd8e1fe34fc1cf308
togher1992 <100290702+togher1992@users.noreply.github.com>
2023-08-01 14:07:17 +0100

    Update vmware_template_finder.py

--- vmware_template_finder.py
+++ vmware_template_finder.py
@@ -18,49 +18,51 @@ class VMwareTemplateFinder(VmwareRestClient):
         self.validate_certs = self.params.get('validate_certs')
         self.username = self.params.get('username')
         self.password = self.params.get('password')
-        self.port = self.params.get('port')
+        self.port = self.params.get('port', '443')  # Default port if not specified
 
         # Session management
         self.session = self.get_vcenter_session()
 
     def api_call(self, url, method='get', headers=None, **kwargs):
-        response = getattr(requests, method)(url, headers=headers, **kwargs)
-        response.raise_for_status()
-        return response.json() if response.text else None
+        try:
+            response = getattr(requests, method)(url, headers=headers, **kwargs)
+            response.raise_for_status()
+            return response.json() if response.text else None
+        except requests.RequestException as e:
+            self.module.fail_json(msg=f"Failed to make API call: {str(e)}")
 
     def get_vcenter_session(self):
-        url = f"https://{self.hostname}/api/session"
+        url = f"https://{self.hostname}:{self.port}/rest/com/vmware/cis/session"
         response = requests.post(url, auth=(self.username, self.password), verify=self.validate_certs)
         response.raise_for_status()
-        return response.json()
+        return response.json().get('value')
 
-    def get_template_attr(self, template_id, attr):
-        url = f"https://{self.hostname}/api/content/library/item/{template_id}"
+    def get_template_attrs(self, template_id, *attrs):
+        url = f"https://{self.hostname}:{self.port}/rest/com/vmware/content/library/item/id:{template_id}"
         headers = {'vmware-api-session-id': self.session}
         data = self.api_call(url, headers=headers, verify=self.validate_certs)
-        return data.get(attr, '') if data else ''
+        return [data['value'].get(attr, '') for attr in attrs] if data else ['' for _ in attrs]
 
     def get_library_id(self):
-        url = f"https://{self.hostname}/api/content/library?action=find"
+        url = f"https://{self.hostname}:{self.port}/rest/com/vmware/content/library?~action=find"
         headers = {'vmware-api-session-id': self.session}
-        params = {'name': self.library, 'type': 'LOCAL'}
+        params = {'spec': {'name': self.library}}
         data = self.api_call(url, method='post', headers=headers, json=params, verify=self.validate_certs)
-        return data[0].replace('"', '') if data else None
+        return data['value'][0] if data and 'value' in data and data['value'] else None
 
     def get_all_template_ids(self, library_id):
-        url = f"https://{self.hostname}/api/content/library/item?library_id={library_id}"
+        url = f"https://{self.hostname}:{self.port}/rest/com/vmware/content/library/item?library_id={library_id}"
         headers = {'vmware-api-session-id': self.session}
         data = self.api_call(url, method='get', headers=headers, verify=self.validate_certs)
         templates = []
         if data:
-            for item in data:
+            for item in data['value']:
                 if item:
                     template_id = item.replace('"', '')
-                    template_name = self.get_template_attr(template_id, 'name')
-                    template_notes = self.get_template_attr(template_id, 'description')
+                    template_name, template_notes = self.get_template_attrs(template_id, 'name', 'description')
                     vm_name = template_name.split('_')[0] if '_' in template_name else template_name
                     templates.append((template_id, vm_name, template_name, template_notes))
-        return templates  # Return an empty list if no templates
+        return templates
 
     def find_template(self, library_id):
         templates = self.get_all_template_ids(library_id)
@@ -73,8 +75,8 @@ class VMwareTemplateFinder(VmwareRestClient):
                     if isinstance(published_status, str):
                         published_status = published_status not in ['False', 'Retired']
                     if published_status and os_version == self.os_version:
-                        return template_name  # Return the name of the first matching template
-        return None  # If no matching templates are found
+                        return template_name
+        return None
 
     def execute(self):
         """Execute module functionality."""
